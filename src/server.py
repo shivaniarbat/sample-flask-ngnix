7ef0884b78bc33c5cc77012371535376c440ed04
shivani <shivani.arbat@gmail.com>
2020-10-14 04:21:10 +0000

    first commit

---
+++ src/server.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, jsonify
+#from pymongo import MongoClient
+
+app = Flask(__name__)
+
+@app.route("/ping")
+def ping():
+    return "pong"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
